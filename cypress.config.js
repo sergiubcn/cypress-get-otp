4077710a7d81c2ae1809f398326ad996a2690471
Sergiu Bacanu <sergiu.bacanu@gmail.com>
2025-09-22 18:15:39 +0300

    chore: updated cy config downloads folder

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    downloadsFolder: "tests/downloads",
     setupNodeEvents() {},
     specPattern: "tests/specs/**/*.spec.js",
     supportFile: "tests/support/e2e.js",
